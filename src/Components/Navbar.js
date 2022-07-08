f7dbae00d2989f1412dea1a03d0abfdc0e29bbdd
Camilo Mayorga Ríos <camilo@MacBook-Air-de-Camilo.local>
2022-07-07 20:02:00 -0400

    iconos añadidos a vista new menu

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBellConcierge } from "@fortawesome/free-solid-svg-icons";
+
 const Navbar = (props) => {
   return <>
   <nav className="navbar">
     <div className="container-fluid">
-      <a className="navbar-brand text-success navbar-title">MM</a>
+      <a className="navbar-brand text-success navbar-title">MM <FontAwesomeIcon icon={faBellConcierge}/></a>
       <span className="navbar-text">{props.title}</span>
       <span className="d-flex">
         <button className="btn btn-outline-success navbar-text" href="#">
