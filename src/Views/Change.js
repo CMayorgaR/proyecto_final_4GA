bf7c976bab0b66791b6cded469c9db46732ec83c
yoselin retamal <yoselin_yar@hotmail.com>
2022-07-27 22:42:42 -0400

    se crea enlace para ir a otra ruta

---
+++ src/Views/Change.js
@@ -0,0 +1,13 @@
+import ChangePassword from '../Components/ChangePassword';
+import Navbar from "../Components/Navbar";
+
+function Change(){
+    return(
+        <>
+            <Navbar title="Cambiar Contraseña" />
+            <ChangePassword />
+        </>
+    );
+}
+
+export default Change;
